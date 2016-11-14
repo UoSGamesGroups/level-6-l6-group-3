578ef7261be3cb2387c811f3d3d0ad8d6361489a
paperjuice <paperjuiceinc@gmail.com>
2016-11-13 16:58:41 -0800

    temple graybox added, brazier and gate coded

---
+++ _SoulLust_UnityProject/Assets/Scripts/Environment/openGateOnTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openGateOnTrigger : MonoBehaviour {
+
+    private Animator _camera;
+    [SerializeField] Animator anim;
+    [SerializeField]private float endTime;
+    private float currentTime;
+    private bool activateShake;
+
+
+    void Awake()
+    {
+        _camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("open");
+            activateShake = true;
+        }
+    }
+
+    void Update()
+    {
+        if (activateShake)
+        {
+            _camera.SetTrigger("shake");
+            currentTime += Time.deltaTime;
+        }
+
+        if (currentTime >=endTime)
+        {
+            currentTime = 0f;
+            activateShake = false;
+        }
+    }
+}
