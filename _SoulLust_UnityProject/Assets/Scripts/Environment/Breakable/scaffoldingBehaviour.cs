449d2bb76dfdefe21546bc2c3d2d8e58867de8ac
paperjuice <paperjuiceinc@gmail.com>
2016-11-21 12:14:28 +0200

    added breakable behaviour, spears and boulders now dmg mc

---
+++ _SoulLust_UnityProject/Assets/Scripts/Environment/Breakable/scaffoldingBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scaffoldingBehaviour : MonoBehaviour {
+
+    [SerializeField] Rigidbody[] rigid;
+    bool isBroken = false;
+
+
+    //void Update()
+    //{
+    //    foreach (Rigidbody r in rigid)
+    //    {
+    //        if (r.velocity == Vector3.zero && isBroken)
+    //        {
+    //            r.isKinematic = true;
+    //        }
+    //    }
+    //}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "weapon")
+        {
+            foreach (Rigidbody r in rigid)
+            {
+                r.isKinematic = false;
+               // isBroken = true;
+            }
+        }
+    }
+}
---
+++ _SoulLust_UnityProject/Assets/Scripts/Environment/Traps/boulderOnPlayerCollisionBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boulderOnPlayerCollisionBehaviour : MonoBehaviour {
+
+    bool isHarmful = true;
+
+    
+    void Update()
+    {
+        if (transform.position.y < -10f)
+        {
+            transform.position = new Vector3()
+        }
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "collider")
+            isHarmful = false;
+
+
+        if (col.gameObject.tag == "Player")
+        {
+            if(isHarmful)
+                col.gameObject.SetActive(false);
+        }
+    }
+
+
+
+}
