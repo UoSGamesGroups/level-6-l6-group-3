cf2ef2a002cb86236cd98cafebd207d347c54845
paperjuice <paperjuiceinc@gmail.com>
2017-02-27 07:59:31 +0000

    meeting minutes update

--- _SoulLust_UnityProject/Assets/Scripts/Environment/Breakable/scaffoldingBehaviour.cs
+++ _SoulLust_UnityProject/Assets/Scripts/Environment/Breakable/scaffoldingBehaviour.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class scaffoldingBehaviour : MonoBehaviour {
 
@@ -24,7 +22,7 @@ public class scaffoldingBehaviour : MonoBehaviour {
                 r.isKinematic = false;
             }
 
-            enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
         }
     }
 }
